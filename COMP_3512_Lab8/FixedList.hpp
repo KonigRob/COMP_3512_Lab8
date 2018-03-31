f317e186c8609d139cac1f4401e83589a710a9ff
Rob Koenig <rkoenig5@my.bcit.ca>
2018-03-30 17:27:23 -0700

    ENJOY YOUR PICKLES!

--- COMP_3512_Lab8/FixedList.hpp
+++ COMP_3512_Lab8/FixedList.hpp
@@ -17,83 +17,128 @@ public:
 	T remove(const T&);
 	/*hi ya rob rob ^w^ */
 private:
-	const size_t capacity = N;
-	size_t size = 0;
+	const size_t bigsize{ N };
+	size_t count{0};
 	T set[N];
 };
 
+//DESC: Constructor for FixedList (which is empty by instructions by instructor and his bear, Pooh))
+//PRE: 
+//POST: 
+//Return:
 template<class T, size_t N>
 inline FixedList<T, N>::FixedList()
 {
-	
 }
 
+//DESC: Destructor, which isn't used.
+//PRE: 
+//POST: 
+//Return:
 template<class T, size_t N>
 inline FixedList<T, N>::~FixedList()
 {
-
 }
 
+//DESC: Get's the number of the pass index as a parameter
+//PRE: All number's are positive
+//POST: 
+//Return: a positive class type, or a exception if out of range
 template<class T, size_t N>
 inline const T & FixedList<T, N>::get(unsigned int index) const
 {
-	if (index > size) {
-		throw std::out_of_range;
+	if (index > count) {
+		throw std::out_of_range ("out of range");
 	}
 	else {
 		return set[index];
 	}
 }
 
+//DESC: overloaded [] operator (look!  I'm commenting!)
+//PRE: Takes in a positive int
+//POST: 
+//Return:
 template<class T, size_t N>
 inline T & FixedList<T, N>::operator[](unsigned int index)
 {
-	return [index];
+	return set[index];
 }
 
+//DESC: Return the first index of a class type
+//PRE: 
+//POST: 
+//Return: the index if found, otherwise -1
 template<class T, size_t N>
 inline int FixedList<T, N>::getFirstIndex(const T & t) const
 {
-	for (int i = 0; i < size; ++i) {
-		if (t == T[i]) {
+	for (int i = 0; i < count; ++i) {
+		if (t == set[i]) {
 			return i;
 		}
 	}
 	return -1;
 }
 
+//DESC: Return the number of elements in the list
+//PRE: 
+//POST: 
+//Return: a number
 template<class T, size_t N>
 inline size_t FixedList<T, N>::size() const
 {
-	return size;
+	return count;
 }
 
+//DESC: Return the max size of the elements that can fit into a pickle jar
+//PRE: 
+//POST: 
+//Return: a number so small, but at the same time so large that Chris himself
+//        questions if he's reading these comments still, or is craving pickles.  
+//        To the fridge chris, the fridgeeeee
 template<class T, size_t N>
 inline size_t FixedList<T, N>::capacity() const
 {
-	return capacity;
+	return bigsize;
 }
 
+//DESC: What happens when Chris doesn't want the pickle he took out?
+//      Then clearly he'll try to add it back into the pickle jar.  But if the amount in there
+//      is already the max size, then he won't be able to fit it in.
+//PRE: 
+//POST: 
+//Return: A decsion.  Does Chris shove the pickle into the pickle jar with 1 hand or 2?  Will be be successful?
 template<class T, size_t N>
 inline bool FixedList<T, N>::add(const T & t)
 {
-	if (size == capacity) {
+	if (count >= bigsize) {
 		return false;
 	}
 	else {
-		set[size] = t;
-		size++;
+		set[count] = t;
+		count++;
 		return true;
 	}
 	
 }
 
+//DESC: Ahha!  I've caught you!  Those pickles were meant for our afternoon snack, but now you want them for lunch?
+//      Well I don't think there will be enough Mister.  Okay, well, if you do grab one, then tell me which one it was. 
+//		If not, then just don't tell me.
+//PRE: 
+//POST: 
+//Return: Which pickle was it!  None you say?  But I see it, behind your back!
 template<class T, size_t N>
 inline T FixedList<T, N>::remove(const T & t)
 {
-	for (int i = 0; i < N; ++i) {
-		if (t == T[i]) {
-			return 
+	for (int i = 0; i < count; ++i) {
+		if (t == set[i]) {
+			int found = set[i];
+			for (int j = i; j < count; ++j) {
+				set[j] = set[j + 1];
+			}
+			return found;
 		}
 	}
+	return NULL;
 }
--- FixedListTester/unittest1.cpp
+++ FixedListTester/unittest1.cpp
@@ -1,5 +1,5 @@
-#include "CppUnitTest.h"
 #include "../COMP_3512_Lab8/FixedList.hpp"
+#include "CppUnitTest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,11 +9,96 @@ namespace FixedListTester
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(get1)
 		{
-			/*FixedList<int, 4> fl;
-			Assert::IsTrue(1, fl.capacity);*/
+			FixedList<int, 4> fl;
+			Assert::ExpectException<std::out_of_range>([&] {fl.get(1); });
 		}
-
+		TEST_METHOD(get2)
+		{
+			FixedList<int, 4> fl;
+			fl.add(2);
+			fl.add(4);
+			Assert::AreEqual(4, fl.get(1));
+		}
+		TEST_METHOD(square1)
+		{
+			FixedList<int, 4> fl;
+			fl.add(2);
+			fl.add(4);
+			Assert::AreEqual(4, fl[1]);
+		}
+		TEST_METHOD(square2)
+		{
+			FixedList<int, 4> fl;
+			fl.add(2);
+			fl.add(4);
+			Assert::AreEqual(2, fl[0]);
+		}
+		TEST_METHOD(getFirstIndex1)
+		{
+			FixedList<int, 4> fl;
+			fl.add(2);
+			fl.add(4);
+			Assert::AreEqual(0, fl.getFirstIndex(2));
+		}
+		TEST_METHOD(getFirstIndex2)
+		{
+			FixedList<int, 4> fl;
+			fl.add(2);
+			fl.add(4);
+			Assert::AreEqual(1, fl.getFirstIndex(4));
+		}
+		TEST_METHOD(size1)
+		{
+			FixedList<int, 4> fl;
+			fl.add(2);
+			fl.add(4);
+			Assert::AreEqual((size_t)2, fl.size());
+		}
+		TEST_METHOD(size2)
+		{
+			FixedList<int, 4> fl;
+			Assert::AreEqual((size_t)0, fl.size());
+		}
+		TEST_METHOD(capacity1)
+		{
+			FixedList<int, 4> fl;
+			Assert::AreEqual((size_t)4, fl.capacity());
+		}
+		TEST_METHOD(capacity2)
+		{
+			FixedList<int, 190> fl;
+			Assert::AreEqual((size_t)190, fl.capacity());
+		}
+		TEST_METHOD(add1)
+		{
+			FixedList<int, 190> fl;
+			Assert::IsTrue(fl.add(4));
+		}
+		TEST_METHOD(add2)
+		{
+			FixedList<int, 0> fl;
+			fl.add(4);
+			Assert::IsFalse(fl.add(1));
+		}
+		TEST_METHOD(remove1)
+		{
+			FixedList<int, 2> fl;
+			fl.add(4);
+			fl.add(2);
+			Assert::AreEqual(2, fl.remove(2));
+		}
+		TEST_METHOD(remove2)
+		{
+			FixedList<int, 6> fl;
+			fl.add(0);
+			fl.add(42);
+			fl.add(13);
+			fl.add(6);
+			fl.add(5);
+			fl.add(3);
+			Assert::AreEqual(NULL, fl.remove(2));
+		}
 	};
 }
